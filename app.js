a3e4746b5410928e3a49bce82a6746a1fe13e891
mahalabsi <mahatalabsi@gmail.com>
2024-11-12 14:58:48 +0300

    html1 updated-button can be clicked

--- app.js
+++ app.js
@@ -1,8 +1,1 @@
 // app.js
-document.addEventListener('DOMContentLoaded', () => {
-  const button = document.getElementById('btn')
-  button.addEventListener('click', () => {
-    window.location.href =
-      'file:///Users/mahalabsi/ga/project1/2048-Game/index2.html' // Change 'game.html' to the actual name of your game HTML file
-  })
-})
