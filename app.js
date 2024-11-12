144839496e86012843a532fe8549bbff8c29c28b
mahalabsi <mahatalabsi@gmail.com>
2024-11-12 12:50:55 +0300

    html2 added

---
+++ app.js
@@ -0,0 +1,8 @@
+// app.js
+document.addEventListener('DOMContentLoaded', () => {
+  const button = document.getElementById('btn')
+  button.addEventListener('click', () => {
+    window.location.href =
+      'file:///Users/mahalabsi/ga/project1/2048-Game/index2.html' // Change 'game.html' to the actual name of your game HTML file
+  })
+})
