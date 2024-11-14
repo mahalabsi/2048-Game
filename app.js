220def93d84afe73025139a27a5b5e80e1d7fe3f
mahalabsi <mahatalabsi@gmail.com>
2024-11-14 11:05:22 +0300

    generate array when the page is loaded

--- app.js
+++ app.js
@@ -1,1 +1,18 @@
-// app.js
+var board
+var score = 0
+var rows = 4
+var columns = 4
+
+const setGame = () => {
+  const board = [
+    [0, 0, 0, 0],
+    [0, 0, 0, 0],
+    [0, 0, 0, 0],
+    [0, 0, 0, 0]
+  ]
+
+  console.log(board)
+}
+
+// Call the setGame function
+window.onload = setGame()
